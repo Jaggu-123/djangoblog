af2768e5807db5f4e478931dd1697bacbf6cd5fc
Jaggu-123 <iit2017503@iiita.ac.in>
2020-08-23 23:23:43 +0530

    Added templates and database(till tutorial 11)

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Articles
 
 def articles_list(request):
-    return render(request, 'articles/artcles_list.html')
+    articles = Articles.objects.all().order_by('date')
+    return render(request, 'articles/artcles_list.html', {'articles': articles})
