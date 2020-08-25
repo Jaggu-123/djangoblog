152ed1d185b39f204260d698893cf2f7612b0a41
Jaggu-123 <iit2017503@iiita.ac.in>
2020-08-25 18:51:54 +0530

    Named Urls(tutorial 15)

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'article'
+
 urlpatterns = [
-    url(r'^$', views.articles_list),
-    url(r'^(?P<slug>[\w-]+)/$', views.articles_detail)
+    url(r'^$', views.articles_list, name='list'),
+    url(r'^(?P<slug>[\w-]+)/$', views.articles_detail, name='detail')
 ]
