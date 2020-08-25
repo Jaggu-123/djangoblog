d97be9645226188ae696d0a4b4c784c1e06abfff
Jaggu-123 <iit2017503@iiita.ac.in>
2020-08-25 18:39:41 +0530

    Extracted the URL parameters

--- articles/urls.py
+++ articles/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.articles_list)
+    url(r'^$', views.articles_list),
+    url(r'^(?P<slug>[\w-]+)/$', views.articles_detail)
 ]
