737bfbbe74f23e20a22e0da7bd970220e466ba2b
Jaggu-123 <iit2017503@iiita.ac.in>
2020-08-26 18:50:40 +0530

    Completed saving articles to db(Tut 27)

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Articles
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from . import forms
 
 def articles_list(request):
     articles = Articles.objects.all().order_by('date')
@@ -13,4 +14,14 @@ def articles_detail(request, slug):
 
 @ login_required(login_url='/accounts/login/')
 def articles_create(request):
-    return render(request, 'articles/articles_create.html')
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST, request.FILES)
+        if form.is_valid():
+            # save to db
+            instance = form.save(commit=False)
+            instance.author = request.user
+            instance.save()
+            return redirect('article:list')
+    else:
+        form = forms.CreateArticle()
+    return render(request, 'articles/articles_create.html', {'form': form})
